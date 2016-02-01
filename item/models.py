90e8db065e3b8bf61da0cf80b29289a3d17d148f
VincentRoma <roma.vincent@gmail.com>
2016-02-01 20:04:57 +0100

    Add offer table to keep track of the market

--- item/models.py
+++ item/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.utils import timezone
+
 
 class Item(models.Model):
     name = models.CharField(max_length=200, null=True)
@@ -7,3 +11,28 @@ class Item(models.Model):
     item_class = models.PositiveIntegerField(null=True)
     sub_class = models.PositiveIntegerField(null=True)
     level = models.PositiveIntegerField(null=True)
+    created_at = models.DateTimeField('now')
+
+
+class Watch(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='watchs')
+
+    price = models.PositiveIntegerField(default=0)
+    inferior_to = models.BooleanField(default=True)
+
+
+class Offer(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='offers')
+
+    seller = models.CharField(max_length=200, null=True)
+    unit_price = models.PositiveIntegerField(null=True)
+    created_at = models.DateTimeField('now')
+
+
+@receiver(pre_save,sender=Item)
+def my_callback(sender, instance, *args, **kwargs):
+    instance.created_at = timezone.now()
+
+@receiver(pre_save,sender=Offer)
+def my_callback(sender, instance, *args, **kwargs):
+    instance.sold_at = timezone.now()
